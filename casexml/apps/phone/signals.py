3412bf8663440abbc5194a6a2280fca0987a05a2
Daniel Roberts <droberts@dimagi.com>
2012-12-12 23:14:13 +0530

    add update_after to some inconsequential db calls that are likely holding up posts; more of this needs to be done

--- casexml/apps/phone/signals.py
+++ casexml/apps/phone/signals.py
@@ -12,18 +12,24 @@ def send_default_response(sender, xform, **kwargs):
     """
     
     def forms_submitted_count(user):
-        forms_submitted = get_db().view("couchforms/by_user", 
-                                        startkey=[user], 
-                                        endkey=[user, {}]).one()
+        forms_submitted = get_db().view(
+            "couchforms/by_user",
+            startkey=[user],
+            endkey=[user, {}],
+            stale='update_after',
+        ).one()
         return forms_submitted["value"] if forms_submitted else "at least 1"
     
     def forms_submitted_today_count(user):
         today = datetime.today()
         startkey = [user, today.year, today.month - 1, today.day]
         endkey = [user, today.year, today.month - 1, today.day, {}]
-        forms_submitted_today = get_db().view("couchforms/by_user", 
-                                              startkey=startkey, 
-                                              endkey=endkey).one()
+        forms_submitted_today = get_db().view(
+            "couchforms/by_user",
+            startkey=startkey,
+            endkey=endkey,
+            stale='update_after',
+        ).one()
         return forms_submitted_today["value"] if forms_submitted_today else "at least 1"
         
     if xform.metadata and xform.metadata.userID:
